f60b702202283787b7e04dcd3509ba8b306ac51b
apoorv-x12 <76410421+apoorv-x12@users.noreply.github.com>
2024-06-28 23:55:30 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,29 +10,29 @@ const Header = () => {
     <header className="flex p-4 justify-between gap-16 ">
         
         <ModeToggle/>
-        <div className="flex justify-evenly gap-16 items-center bg-clip-text text-transparent bg-gradient-to-r from-orange-1 to-orange-2 text-xl text font-bold">
-            <div>
+        <div className="flex flex-wrap justify-evenly gap-16 items-center bg-clip-text text-transparent bg-gradient-to-r from-orange-1 to-orange-2 text-xl text font-bold">
+            <div className='hover:bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
                 <Link to={'/home'}>
                   Home
                 </Link>
             </div>
-            <div>
+            <div className='hover:bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
                 <Link to={'/package'}>
                   Package
                 </Link>
             </div>
-            <div>
+            <div className='hover:bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
                 <Link to={'/compare'}>
                   Compare
                 </Link>
             </div>
-            <div >
+            <div className='hover:bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
                 <Link to={'/genAi'}>
                   GenAi
                 </Link>
             </div>
         </div>
-        <a className='animate-bounce hover:animate-pulse' rel='noopener' target='_blank' href="http://www.npmjs.com">
+        <a className='animate-pulse bg-blue-500 dark:bg-red-500 rounded-md' rel='noopener' target='_blank' href="http://www.npmjs.com">
         <Avatar>
             <AvatarImage src={npm} />
             <AvatarFallback>NPM</AvatarFallback>
