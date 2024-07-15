ae9e59680ee08fc95dcbff6bb77acc43a4824a27
apoorv-x12 <76410421+apoorv-x12@users.noreply.github.com>
2024-07-15 06:58:18 +0530

    Added Acceternity UI and Home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,36 @@
 
+import { Meteors } from '../components/ui/meteors';
+import { TextGenerateEffect } from '../components/ui/text-generate-effect';
+import { TypewriterEffect } from '../components/ui/typewriter-effect';
+
+
 const Home = () => {
+    const words = [
+    {
+      text: "Welcome",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+    {
+      text: "to",
+      className: "text-red-500 dark:text-red-500",
+    },
+    {
+      text: "npm-stats-viz",
+      className: "text-yellow-500 dark:text-yellow-500",
+    },
+    
+
+  ];
+
   return (
     <div>
-      Home
+      <div className='flex flex-col justify-center items-center gap-4 m-8'>
+              <TypewriterEffect words={words}/>
+              <TextGenerateEffect words={"Find out the stats of any npm package !"}/>
+              <Meteors number={50} className='ml-96'/>
+      </div>
+     
+    
     </div>
   )
 }
