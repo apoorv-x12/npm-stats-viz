d71f61acb29ab928d315847b60c05e84f6b37d40
apoorv-x12 <76410421+apoorv-x12@users.noreply.github.com>
2024-07-21 17:08:33 +0530

    attempt 3 deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/npm-stats-viz",
+  base: "/npm-stats-viz/",
   plugins: [ react(),
   ],
    resolve: {
