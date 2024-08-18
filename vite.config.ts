17d42fef41c4737c25000b8a891ec0e80ba054ae
apoorv-x12 <fifa.ajay219@gmail.com>
2024-08-18 18:20:45 +0530

    trying to fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react(),],
-  base: "./",
+  base: "/npm-stats-viz/",
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
