121e8b9212029521b1d7bd49868061ce6debd9ca
apoorv-x12 <fifa.ajay219@gmail.com>
2024-08-18 18:44:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react(),],
-  base: "./",
+  base: "/",
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
